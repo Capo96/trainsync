64214b9632de0525b918bd560e9a245877e01112
Capo96 <miguelsimone9600@gmail.com>
2026-05-17 21:30:59 +0200

    notion.js

--- netlify/functions/notion.js
+++ netlify/functions/notion.js
@@ -1,7 +1,7 @@
 const NOTION_TOKEN = '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';
 const STRAVA_CLIENT_ID = '246761';
 const STRAVA_CLIENT_SECRET = '824d5d4abbf22903c5db6014e2a5a9892ed1daeb';
-const STRAVA_REFRESH_TOKEN = 'e21590e96d19cdcd06633ffe91098ac25a12b533';
+const STRAVA_REFRESH_TOKEN = '4323ddfbb86930d560054c101b1048fc3cbf930f';
 
 exports.handler = async function(event) {
   const headers = {
@@ -12,28 +12,6 @@ exports.handler = async function(event) {
 
   const action = event.queryStringParameters?.action || '';
 
-  // Exchange code for tokens
-  if (action === 'exchange') {
-    const code = event.queryStringParameters?.code || '';
-    try {
-      const resp = await fetch('https://www.strava.com/oauth/token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          client_id: STRAVA_CLIENT_ID,
-          client_secret: STRAVA_CLIENT_SECRET,
-          code: code,
-          grant_type: 'authorization_code'
-        })
-      });
-      const data = await resp.json();
-      return { statusCode: resp.status, headers, body: JSON.stringify(data) };
-    } catch(e) {
-      return { statusCode: 500, headers, body: JSON.stringify({ error: e.message }) };
-    }
-  }
-
-  // Refresh access token
   if (action === 'strava_refresh') {
     try {
       const resp = await fetch('https://www.strava.com/oauth/token', {
@@ -53,7 +31,6 @@ exports.handler = async function(event) {
     }
   }
 
-  // Notion proxy
   const endpoint = event.queryStringParameters?.endpoint || '';
   if (endpoint) {
     try {
